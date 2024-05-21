8a7be760095beed5326c17d6b3cd974aec363827
Callum Styan <callumstyan@gmail.com>
2024-05-21 11:05:27 -0700

    update readme and add script
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # Monsters & Memories Linux Setup
+
 Native Linux setup and installation instructions for the game Monsters & Memories.
 
+Note that so far we have only tested getting the launcher to run to the point of being able to login and create a characer. Neither of us were around for earlier playtests, so this will likely be updated based on the upcomming June stress/playtests.
+
+# General Requirements
+
+## System Requirements 
+- Modern CPU (2017+)
+- 16GB of memory DDR4 or higher (2133MT+).
+- Any GPU or integrated GPU that supports DirectX 11
+
+## Distribution Requirements
+- Kernel 6.1 LTS or Newer
+- GUI / Desktop Environment
+
+# Required Packages
+
+To get M&M to run natively on Linux you need a few packages that you may not have installed already if you haven't been playing games on Linux.
+
+## Arch
+
+On Arch, your best bet is to follow the Arch Wiki for installing Steam [here](https://wiki.archlinux.org/title/steam). You'll need to enable multilib packages to install Steam via `pacman`.
+
+After that you want to ensure you have the following:
+- [mesa](https://archlinux.org/packages/extra/x86_64/mesa/)
+- [32bit OpenGL driver](https://wiki.archlinux.org/title/Xorg#Driver_installation), check the table on this page for the best option for your GPU (for me this was `xf86-video-amdgpu`)
+- [Vulkan](https://wiki.archlinux.org/title/Vulkan#Installation), again check the page for which exact package(s) are options for your GPU (I used [vulkan-radeon](https://archlinux.org/packages/?name=vulkan-radeon))
+- [proton](https://aur.archlinux.org/packages/proton), you can use the AUR package or alternatively use our setup script to get the latest release from proton's git repo. If you use the AUR pacakge you need to configure the `mnmlauncher.exe` in Steam's compatability settings to use `proton (Unsupported AUR package [native])` (which I have tested, and still works).
+
+# Install script
+
+The install script will prompt you a few times for your distribution info and some other questions, and install some relevant packages + start Steam. Note that currently in the script you need to modify the download link line with your account tag, which you can find by logging into `https://account.monstersandmemories.com`.
+
+# Add the game to Steam + Proton
+
+We have to configure `mnmlauncher.exe` to execute via Proton from steam, to do this:
+- within steam `Games` click `Add a Non-Steam Game to My Library` and naviagte to the download location of mnmlauncher.exe
+- once added go to the Library tab and right click the game and select properties. 
+- next click `Compatibility` and check the "Force the use of a specifoc Steam Play compatibility tools" box
+- finally Select the Proton version that starts with `GE`. It should look something like `GE-Proton9-5`
+  - if using the AUR package select `proton (Unsupported AUR package [native])`
---
+++ install-nmn.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# This will help you install the MMORPG Monsters & Memories on Linux (https://monstersandmemories.com/)
+# Made by Wyatt (3930kfc) and Callum (cstyan) from the Monsters & Memories Discord server. (https://discord.com/invite/jvdKKc3CqW)
+
+# 1. Install Steam
+ARCH=false
+ANSWER=""
+echo "Are you using an Arch based Linux distribution? [y/n]"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then 
+    ARCH=true
+    echo "Have you updated your /etc/pacman.conf to include the mirrorlist for multilib?"
+    read ANSWER
+    if [ "$ANSWER" == "y" ]; then 
+        sudo pacman -Syu steam # Download Steam for Arch & Manjaro Linux
+    else
+        echo "You must have a mirrorlist config for multilib for steam to be installed properly, see: https://wiki.archlinux.org/title/steam"
+        exit 1
+    fi
+elif [ "$ANSWER" == "n" ]; then
+    echo "Are you using an Debian based Linux distribution? [y/n]"
+    read ANSWER
+    if [ "$ANSWER" == "y" ]; then 
+        sudo apt-get update
+        sudo apt-get install -y steam # Download Steam for Debian, Ubuntu, and Mint Linux
+    elif [ "$ANSWER" == "n" ]; then
+        echo "Are you using a Fedora based Linux distribution? [y/n]"
+        read ANSWER
+        if [ "$ANSWER" == "y" ]; then 
+            sudo dnf install -y steam # Download Steam for Fedora Linux
+        fi
+    fi
+fi
+
+# 2. Download and install Proton GE from it's github repository "https://github.com/GloriousEggroll/proton-ge-custom".
+install_proton_source() {
+    mkdir "$HOME/.local/share/Steam/" # Make Directory
+    mkdir "$HOME/.local/share/Steam/compatibilitytools.d/" # Make Directory
+    cd "$HOME/.local/share/Steam/compatibilitytools.d/" # Change Directory
+    wget --secure-protocol=TLSv1_3 --retry-connrefused --waitretry=3 https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest -O - | awk -F \" -v RS="," '/browser_download_url/ {print $(NF-1)}' | xargs wget # Download Proton GE 
+    tar -zxf "GE-Proton"*".tar.gz" # Unzip GE-Proton
+    rm "GE-Proton"*".tar.gz" # Remove Zipfilerm "GE-Proton"*".tar.gz" # Remove Zipfile
+
+    # 3. Download Monsters and Memories
+    mkdir $HOME/Games/ # Make Directory
+    mkdir $HOME/Games/"Monsters & Memories"/ # Make Directory
+    cd $HOME/Games/"Monsters & Memories"/ # Change Directory
+    wget --secure-protocol=TLSv1_3 --retry-connrefused --waitretry=3 -O "Monsters & Memories.zip" "https://account.monstersandmemories.com/*/mnmlauncher.zip"
+    unzip "mnmlauncher.zip" # Unzip Launcher
+    rm "mnmlauncher.zip" # Remove Zipfile
+}
+
+
+if [ "$ARCH" = true]; then
+    echo "Would you like to install the proton AUR package rather than installing proton from source?"
+    read ANSWER
+    if [ "$ANSWER" == "y" ]; then 
+        echo "installing via pacman"
+        pacman -S proton
+    else
+        install_proton_source
+    fi
+else 
+   install_proton_source
+fi
+
+# 4. Login to Steam and Select Proton-GE for Monsters & Memories
+steam & # Starts Steam in the background
+echo -e "\n"'Please login to Steam and click the top "Games" tab. From there click "Add a Non-Steam Game to My Library" and naviagte to the download location of mnmlauncher.exe. Once added go to the Library tab and right click the game and select properties. 
+
+From there go to "Compatibility" and check the "Force the use of a specofoc Steam Play compatibility tools" box. From there Select the Proton version that starts with "GE". It should look something like "GE-Proton9-5".'
